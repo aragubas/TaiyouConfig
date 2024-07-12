e0c639b52536c74dc06c4b2dfd72eab7ac527a38
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-12 18:13:24 -0300

    Removed every single file

--- TaiyouConfig/src/Linker/Link.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include "UncompiledNamespace.hpp"
-#include "LinkedTcfgUnit.hpp"
-
-namespace TaiyouConfig::Linker
-{
-	LinkedTcfgUnit LinkUnits(std::vector<TcfgUnit> &units);
-}
